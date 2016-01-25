360dfa7572527952b954234105a8827538881f3b
Akiva Gordon <akivag29@gmail.com>
2016-01-25 15:36:53 -0500

    Refactor `urlify` for a simpler implementation

--- urlify.js
+++ urlify.js
@@ -4,19 +4,12 @@
     "use strict";
 
     module.exports = function (str) {
-        const joinedAlphaNumeric = str
+        return str
             .toLowerCase()
-            .replace(/[^a-z0-9]/g, '-')
-            .replace(/-+/g, '-');
-
-        const startIndex = joinedAlphaNumeric.startsWith('-')
-            ? 1
-            : 0;
-
-        const endIndex = joinedAlphaNumeric.endsWith('-')
-            ? -1
-            : Infinity;
-
-        return joinedAlphaNumeric.slice(startIndex, endIndex);
+            .replace(/[^a-z0-9]/g, ' ')
+            .replace(/\s+/g, ' ')
+            .trim()
+            .split(' ')
+            .join('-');
     };
 }());
